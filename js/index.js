88d1df995e32704eb8e7f23af10ce853d2b12ae5
Alexis <alexis.jonas@gmail.com>
2020-07-30 12:43:39 -0600

    layout changes made

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
 const icon = document.querySelector(".card__share");
-const icon2 = document.querySelector(".card__shareActive .card__share");
+const iconActive = document.querySelector(".card__shareActive .card__share");
 const popup = document.getElementById("share-popup");
+const triangle = document.querySelector(".triangle");
 
 icon.addEventListener('click', function() {
     popup.classList.toggle("card__shareHidden");
+    
+    if (!popup.matches(".card__shareHidden")) {
+        triangle.classList.toggle("triangle__hidden");
+    } else {
+        triangle.classList.toggle("triangle__hidden");
+    }
 })
 
-icon2.addEventListener('click', function() {
+iconActive.addEventListener('click', function() {
     popup.classList.toggle("card__shareHidden");
 })
 
